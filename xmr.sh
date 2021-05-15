1aaf3682f3ece5bc283c664e086ade5b80d0446f
salmo007 <83998996+salmo007@users.noreply.github.com>
2021-05-15 11:48:45 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,4 @@
+apt update && upgrade
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig-6.12.1/xmrig --no-color --donate-level 0 -o pool.hashvault.pro:80 -u hvxxy7rV9vmXZKUsWVpKFxKyTG1Zp2SfqYRv5hnxjXANSUMFmPLzQvG55q9innTqpDRTaZTdecdGQNh23XMecjAA2hSnK3rD8o -p x -a cn-heavy/xhv -k --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
